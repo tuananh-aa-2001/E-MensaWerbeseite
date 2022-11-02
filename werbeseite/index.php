1b4637aa5b16bed0b28e902d598cafc2fe68ff1e
tuan_anh <nguyentuan6730@gmail.com>
2022-11-02 16:44:48 +0100

    finished task 6

---
+++ werbeseite/index.php
@@ -0,0 +1,183 @@
+<!DOCTYPE html>
+<!--
+- Praktikum DBWT. Autoren:
+- Tuan,Nguyen, 3517392
+- Dorian,Hoevelmann, 3525346
+-->
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Ihre E-Mensa</title>
+    <link rel="stylesheet" type="text/css" href="index_stylesheet.css" >
+</head>
+<body>
+<!--begin header-->
+<!--to do: Links!-->
+  <div id="quickLinks">
+    <div class="logoimg">
+      <img src="MensaLogo.png" height="100" alt="Logo">
+    </div>
+    <a class="announcements" href="#announcementsMain">
+      Ankündigungen
+    </a>
+    <a class="meals" href="#mealsMain">
+      Speisen
+    </a>
+    <a class="numbers" href="#numbersMain">
+      Zahlen
+    </a>
+    <a class="contact" href="#contactMain">
+      Kontakt
+    </a>
+    <a class="important" href="#importantMain">
+      Wichtig für uns
+    </a>
+    </div>
+<!--begin body-->
+<div id="filler">
+  <p class="randMensa">
+    <img src="mensa.jpg" height="275" alt="Mensa Bild">
+  </p>
+</div>
+<div id="announcementsMain">
+  <h1>Bald gibt es Essen auch online ;)</h1>
+  <p class="announcementText">
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque dignissim ultricies turpis, at efficitur justo tempor a. Maecenas porta eget lectus id molestie. Ut aliquet mattis est, nec vehicula mauris maximus ac. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Aenean interdum nunc ut diam bibendum aliquam. Nullam congue erat eget luctus viverra. Sed vulputate tellus et placerat sodales. Maecenas nec aliquet odio. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi lacinia placerat mi quis vestibulum. Suspendisse laoreet neque in porttitor blandit. Integer non blandit nibh. Morbi consectetur, nibh eget volutpat sodales, leo dolor mattis massa, id porttitor massa est a ante.
+  </p>
+</div>
+<div id="mealsMain">
+  <h1>Köstlichkeiten, die Sie erwarten</h1>
+  <table class="mealTable">
+      <thead>
+            <tr>
+                <th> </th>
+                <th>Preis intern</th>
+                <th>Preis extern</th>
+            </tr>
+      </thead>
+      <?php
+      $file = fopen('Gerichte.txt', 'r');
+      if (!$file)
+          echo "Es gibt noch keinen Gericht. Bitte kommen Sie später zurück!";
+      else {
+          while (!feof($file)) {
+              $gerichte = fgets($file);
+
+              if (!empty($gerichte)) {
+
+                  $gerichtelemente = explode('; ', $gerichte);
+
+                  echo '<tr>';
+
+                  foreach ($gerichtelemente as $key => $gerichtelement) {
+                      if ($key === 0)
+                          echo '<td><img alt="Dish_image" width="200px" src="./img/' . htmlspecialchars($gerichtelement) . ' "></td>';
+                      else
+                          echo '<td>' . htmlspecialchars($gerichtelement) . '</td>';
+                  }
+                  echo '</tr>';
+              }
+          }
+          fclose($file);
+      }
+      ?>
+  </table>
+</table>
+</div>
+<div id="numbersMain">
+  <h1>E-Mensa in Zahlen</h1>
+  <ul class="zahlenList">
+    <li>X</li>
+    <li>Besuche</li>
+    <li>Y</li>
+    <li>Anmeldungen zum Newsletter</li>
+    <li>Z</li>
+    <li>Speisen</li>
+  </ul>
+</div>
+<div id="contactMain">
+  <h1>Interesse geweckt? Wir informieren Sie!</h1>
+  <form  method="post" class="contactForm">
+    <fieldset>
+        <p> <label for="name">Ihr Name:</label>
+           <input type="text" id="name" name="name" placeholder="Vorname" required>
+        </p>
+        <p><label for="email">Ihre E-Mail</label>
+            <input type="text" id="email" name="email" placeholder="E-Mail" required>
+        </p>
+        <div id="div_language">
+          <label for="lang">Newsletter bitte in:</label>
+          <select id="lang" name="language">
+            <option value="de">Deutsch</option>
+            <option value="en">English</option>
+            <option value="af">Afrikaans</option>
+          </select>
+        </div>
+       <p>
+         <label for="dsgvo">Den Datenschutzhinweise Stimme ich zu</label>
+         <input type="checkbox" id="dsgvo">
+       </p>
+        <input type="submit" name="submit" value="Zum Newsletter anmelden">
+    </fieldset>
+    <br>
+      <?php
+      $invalid_emails = [
+          1 =>'rcpt.at',
+          2 => 'damnthespam.at',
+          3 => 'wegwerfmail.de',
+          4 => 'trashmail.'
+      ];
+      $file = fopen("Anmeldungsdaten.txt", "a");
+      if ($_POST['submit']) {
+        $errors = [];
+        if(empty(trim($_POST['name'])))
+            $errors[] = 'Bitte geben Sie einen Namen ein!';
+        if(empty($_POST['email']))
+            $errors[] = 'Bitte geben Sie eine gültige Email ein!';
+        else
+            if(!filter_var($_POST['email'], FILTER_VALIDATE_EMAIL))
+                $errors[] = 'Bitte geben Sie eine gültige Email ein!';
+        for($i = 0; $i < count($invalid_emails);$i++){
+            $invalid = strstr($_POST['email'],$invalid_emails[$i+1]);
+            if($invalid)
+                $errors[] = 'Bitte geben Sie eine gültige Email ein!';
+        }
+        if(empty($errors)){
+           echo  '<div class="success"> Vielen Danke. Sie haben sich erfolgreich zum Newsletter angemeldet.</div>';
+           fwrite($file,$_POST['name'].';'.$_POST['email'].';'. $_POST['language'] . "\n");
+        }else{
+            echo '<div class="error"> <ul>';
+            foreach ($errors as $error)
+                echo '<li>'. htmlspecialchars($error).'</li>';
+            echo '</ul> </div>';
+        }
+      }
+      fclose($file);
+      ?>
+
+  </form>
+</div>
+<div id="importantMain">
+  <h1>Das ist uns wichtig</h1>
+  <ul class="importantList">
+    <li>Beste frische saisonale Zutaten</li>
+    <li>Ausgewogene abwechslungsreiche Gerichte</li>
+    <li>Sauberkeit</li>
+  </ul>
+</div>
+<div id="notQuiteFooter">
+  <h1>Wir freuen uns auf Ihren Besuch!</h1>
+</div>
+
+</body>
+<footer>
+  <div>
+    <!--begin footer-->
+    <ul class="unten">
+      <li>(c) E-Mensa GmbH</li>
+      <li>Tuan Nguyen & Dorian Hövelmann</li>
+      <li><a href="#impressum">Impressum</a></li>
+    </ul>
+  </div>
+</footer>
+</html>
